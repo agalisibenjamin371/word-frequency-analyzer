b5f4168cb8c023195b7f3a1490415f7079a79997
Code crush <agalisibenjamin371@gmail.com>
2026-03-16 10:58:43 -0700

    Add files via upload

---
+++ learn.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+
+int number;
+printf("Enter your nombr of choice  :");
+scanf("%d",&number);
+if (number%2==0)
+{
+    printf("the  number is an even number \n");
+}
+    else
+    {
+        printf("the number is an odd number");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
